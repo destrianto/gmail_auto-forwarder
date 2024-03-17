681660d99346f171ce68034e9f041bd7c0b856a7
Ade Destrianto <ade.destrianto@mail.ru>
2024-03-17 22:29:32 +0700

    Version 1.1 build 20240317

---
+++ CHANGELOG.md
@@ -0,0 +1,11 @@
+## Changelog for Gmail Auto-forwarding
+- **Version 1.1 build 20240317**
+```
+- Fixed paging trigger sometimes not called
+- Add a debug option in the configuration
+- Logging improvement
+```
+- **Version 1.0 build 20240307**
+```
+The initial release
+```
--- gmail_auto-forwarder.js
+++ gmail_auto-forwarder.js
@@ -2,8 +2,9 @@
 // CONFIGURATION //
 ///////////////////
 
-// Sample list of filters and recipients. The first element of the array contains the filter and the last element is a list that stores the recipients
+// Sample list of filters and recipients
 var PASS = [
+  // The first element of the array contains the filter, and the last element is an array that stores the recipients
   ['from:"from_me@outlook.com" AND subject:my_outlook', ['to_you@yahoo.com', 'to_you@gmail.com']],
   ['from:"from_me@yahoo.com" AND subject:my_yahoo', ['to_you@outlook.com', 'to_you@gmail.com']],
   ['from:"from_me@gmail.com" AND subject:my_gmail', ['to_you@outlook.com', 'to_you@yahoo.com']]
@@ -25,10 +26,14 @@ var PAUSE = 0.002
 // DEFAULT: 100 messages
 var THREADS = 100
 
+// Set 'true' to enable the complete log
+// DEFAULT: false
+var DEBUG = false
+
 //////////////////////////////////////////////////////////////////
 // AUTHOR  : ADE DESTRIANTO                                     //
 // TITLE   : GMAIL AUTO-FORWARDER                               //
-// VERSION : 1.0 BUILD 20240307                                 //
+// VERSION : 1.1 BUILD 20240317                                 //
 // GITHUB  : https://github.com/destrianto/gmail_auto-forwarder //
 //////////////////////////////////////////////////////////////////
 
@@ -39,7 +44,7 @@ function setTrigger(){
     .timeBased()
     .everyMinutes(CLOCK)
     .create()
-  console.info('The forwarder\'s clock trigger in every ' + CLOCK + ' minute(s) has been set')
+  console.info('The forwarder\'s clock trigger is adjusted every ' + CLOCK + ' minute(s)')
 }
 
 // Unset trigger
@@ -67,7 +72,7 @@ function unsetTrigger(trigger = 'unset'){
   // Output appeared after unsetting all kinds of this script's triggers
   if(trigger === 'unset'){
     if(found === 0){
-      console.info('No forwarder\'s trigger has been set')
+      console.info('There is no trigger on set')
     }
     else{
       console.warn('Every forwarder\'s trigger has been unset')
@@ -90,7 +95,7 @@ function waitPager(paging = false){
       }
       // Postpone the new trigger if the paging trigger is active
       else{
-        console.warn('   POSTPONED: The paging batch still in progress')
+        console.warn('   POSTPONED: The paging batch is still in progress')
         return true
       }
     }
@@ -99,7 +104,7 @@ function waitPager(paging = false){
 }
 
 // Set a paging trigger with delay
-function threadsPager(minute = DELAY){
+function threadsPager(minute = DELAY, paged){
   // Set a paging trigger only when there's no other active paging trigger
   if(!waitPager(true)){
     ScriptApp
@@ -107,7 +112,7 @@ function threadsPager(minute = DELAY){
       .timeBased()
       .at(new Date((new Date()).getTime() + 1000 * 60 * minute))
       .create()
-    console.warn('       PAGED: The maximum messages exceeded and will trigger a new forwarder batch in ' + minute + ' minute(s)')
+    console.warn('       PAGED: There are ' + paged + ' job(s) with maximum messages exceeded, and it will trigger a new batch in ' + minute + ' minute(s)')
   }
 }
 
@@ -123,6 +128,7 @@ function forwarder(paging = false){
     return
   }
   else{
+    var count = []
     // Iterate the list of filters and recipients to perform forwarding
     for(var i = 0; i < PASS.length; i++){
       var job = '[Job ' + (i + 1) + ']'
@@ -131,6 +137,7 @@ function forwarder(paging = false){
       // Skip this one filter process due to no matching message(s) found
       if(threads.length === 0){
         console.info('        IDLE: ' + job + ' There are no message(s) that match the query')
+        count.push(false)
         continue
       }
       // Proceed to forward the matching message(s)
@@ -142,22 +149,39 @@ function forwarder(paging = false){
           // Forward the matching message from the Inbox and delete it permanently
           inbox[0].forward(PASS[i][1], {name: name, replyTo: name, subject: inbox[0].getSubject(), htmlBody: inbox[0].getBody()})
           Gmail.Users.Messages.remove('me', inbox[0].getId())
-          // Debug to verify the job on Inbox and Sent are in sequential sync
-          // console.error('       DEBUG: ' + job + ' ### [Inbox: ' + threads.length + ' message(s)] ### [Forward & Delete: Message ' + (j + 1) + '] ### [Subject: \"' + inbox[0].getSubject() + '\"]')
+          // Debug to verify the job between the Inbox and Sent are in sequential sync
+          if(DEBUG){
+            console.warn('       DEBUG: ' + job + ' ### [Inbox] ### [Found: ' + (threads.length - j) + ' message(s)] ### [Forwarding & Deleting: 1 of ' + (threads.length - j) + ' message(s)] ### [Subject: \"' + inbox[0].getSubject() + '\"]')
+          }
           Utilities.sleep(1000 * 60 * PAUSE)
           // Delete the forwarded message from Sent permanently
           var sent = GmailApp.search('in:sent ' + PASS[i][0], 0, THREADS)[0].getMessages()
           Gmail.Users.Messages.remove('me', sent[0].getId())
-          // Debug to verify the job on Inbox and Sent are in sequential sync
-          // console.error('       DEBUG: ' + job + ' ### [Sent: ' + sent.length + ' message(s)] ### [Forward & Delete: Message ' + (j + 1) + '] ### [Subject: \"' + sent[0].getSubject() + '\"]')
+          // Debug to verify the job between the Inbox and Sent are in sequential sync
+          if(DEBUG){
+            console.warn('       DEBUG: ' + job + ' ### [ Sent] ### [Found: ' + sent.length + ' message(s)] ### [             Deleting: 1 of ' + sent.length + ' message(s)] ### [Subject: \"' + sent[0].getSubject() + '\"]')
+          }
         }
         console.info('FORWARDED_TO: ' + job + ' ' + PASS[(i)][1].join(', '))
-        console.info('        DONE: ' + job + ' There are ' + threads.length + ' message(s) was forwarded then permanently deleted')
+        console.info('        DONE: ' + job + ' There are ' + threads.length + ' message(s) are forwarded then permanently deleted')
+        if(threads.length === THREADS){
+          count.push(threads.length)
+        }
+        else{
+          count.push(true)
+        }
       }
     }
     // Set a paging trigger when the number of messages reaches the limit
-    if(threads.length === THREADS){
-      threadsPager(DELAY)
+    if(count.includes(THREADS)){
+      var paged = 0
+      // Count every paged job
+      for(var i = 0; i < count.length; i++){
+        if(count[i] === THREADS){
+          paged++
+        }
+      }
+      threadsPager(DELAY, paged)
     }
     else{
       // Remove the disabled paging trigger at the end of this paging batch
@@ -165,11 +189,11 @@ function forwarder(paging = false){
         unsetTrigger('forwarderPager')
       }
       // Output appeared after every message was filtered
-      if(threads.length === 0){
-        console.info('JOB_FINISHED: No messages was match the queries')
+      if(count.includes(true)){
+        console.info('JOB_FINISHED: All messages forwarded')
       }
       else{
-        console.info('JOB_FINISHED: All messages was forwarded then permanently deleted')
+        console.info('JOB_FINISHED: No messages matched the queries')
       }
     }
   }
